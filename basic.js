1cee633eaed55f66d298b88f7c94fa691b404847
mlciagathungu <mlciagathungu1022@gmail.com>
2025-08-12 13:02:01 +0300

    Initial commit

---
+++ basic.js
@@ -0,0 +1,10 @@
+ function bt(){
+    // alert("Hello World")
+    confirm("Are you sure?")
+    }
+    function changecolor(){
+    document.getElementById("mybtn").style.backgroundColor="lightblue"
+    }
+    function changepara(){
+    document.getElementById("para").style.color="blue"
+    }
